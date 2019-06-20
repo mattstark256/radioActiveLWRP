0661af8c371f6db4f093d68bb28680c058532128
Niklas Evertsson <niklas.evertsson@gmail.com>
2019-06-20 15:30:13 +0100

    Fix a few bugs in pickup

--- radioActiveLWRP/Assets/Scripts/Interactor.cs
+++ radioActiveLWRP/Assets/Scripts/Interactor.cs
@@ -2,21 +2,34 @@
 
 public class Interactor : MonoBehaviour
 {
+    bool isInteracting;
     IInteractable interactable;
 
     public void OnInteractionFinished()
     {
         interactable = null;
+        isInteracting = false;
     }
 
     void OnTriggerEnter(Collider other)
     {
-        interactable = other.GetComponent<IInteractable>();
+        IInteractable otherInteractable = other.GetComponent<IInteractable>();
+        if(otherInteractable != null)
+            interactable = otherInteractable;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(!isInteracting && other.GetComponent<IInteractable>() != null)
+            interactable = null;
     }
 
     void Update()
     {
-        if(Input.GetButtonDown("Interact"))
-            interactable?.Interact(this);
+        if(Input.GetButtonDown("Interact") && interactable != null)
+        {
+            isInteracting = true;
+            interactable.Interact(this);
+        }
     }
 }
--- radioActiveLWRP/Assets/Scripts/Pickup.cs
+++ radioActiveLWRP/Assets/Scripts/Pickup.cs
@@ -2,14 +2,8 @@
 
 public class Pickup : MonoBehaviour, IInteractable
 {
+    float dropDistance = 2.0f;
     Rigidbody rb;
-    Vector3 relativePositionBeforePickup;
-    [FMODUnity.EventRef]
-    [SerializeField]
-    private string _grabEvent;
-    [FMODUnity.EventRef]
-    [SerializeField]
-    private string _placeEvent;
 
     void Awake()
     {
@@ -26,25 +20,17 @@ public class Pickup : MonoBehaviour, IInteractable
 
     void AttachToInteractor(Interactor interactor)
     {
-        relativePositionBeforePickup = transform.position - interactor.transform.position;
-        relativePositionBeforePickup = Vector3.ProjectOnPlane(relativePositionBeforePickup, Vector3.up);
         rb.isKinematic = true;
         transform.parent = interactor.transform;
         transform.SetPositionAndRotation(interactor.transform.position, interactor.transform.rotation);
-        FMODUnity.RuntimeManager.PlayOneShot(_grabEvent);
     }
 
     void DetachFromInteractor(Interactor interactor)
     {
         rb.isKinematic = false;
         transform.parent = null;
-        Vector3 returnPosition = interactor.transform.TransformPoint(relativePositionBeforePickup);
-        transform.SetPositionAndRotation(returnPosition, Quaternion.identity);
+        transform.position = interactor.transform.parent.TransformPoint(Vector3.forward * dropDistance);
+        transform.rotation = Quaternion.identity;
         interactor.OnInteractionFinished();
-    }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        FMODUnity.RuntimeManager.PlayOneShotAttached(_placeEvent, this.gameObject);
     }
 }
