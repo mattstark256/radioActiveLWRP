495ba4bb4618473fe861cb4c1676c597054736c9
Matt Laptop <1800715@uad.ac.uk>
2019-06-21 12:45:21 +0100

    changed corruption tester

--- radioActiveLWRP/Assets/Scripts/CorruptionTester.cs
+++ radioActiveLWRP/Assets/Scripts/CorruptionTester.cs
@@ -19,11 +19,27 @@ public class CorruptionTester : MonoBehaviour
 
     private void Start()
     {
-        corruption.SetCentre(corruptionCentreTransform.position);
+        //corruption.SetCentre(corruptionCentreTransform.position);
+
+        StartCoroutine(TestCorruption());
     }
 
-    void Update()
+    //void Update()
+    //{
+    //    corruption.SetRadius(Mathf.Lerp(radiusRange.x, radiusRange.y, (Mathf.Sin(Time.time * Mathf.PI * 2 / duration) + 1) / 2));
+    //}
+
+    private IEnumerator TestCorruption()
     {
-        corruption.SetRadius(Mathf.Lerp(radiusRange.x, radiusRange.y, (Mathf.Sin(Time.time * Mathf.PI * 2 / duration) + 1) / 2));
+        yield return new WaitForSeconds(1);
+
+        corruption.ExpandFromPoint(corruptionCentreTransform.position, 10, 20);
+
+        yield return new WaitForSeconds(8);
+
+
+        corruption.Dissappear(1);
+        corruption.ExpandFromPoint(corruptionCentreTransform.position + Vector3.forward * 9, 10, 20);
+
     }
 }
