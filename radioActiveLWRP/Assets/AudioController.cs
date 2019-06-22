c0a5cb5f01ea904747c7d860b716f66fe632b23d
Michał Pukała <1703168@uad.ac.uk>
2019-06-22 14:17:30 +0100

    Slight volume tweaks. Added background music and ambience

--- radioActiveLWRP/Assets/AudioController.cs
+++ radioActiveLWRP/Assets/AudioController.cs
@@ -7,7 +7,15 @@ public class AudioController : MonoBehaviour
     [FMODUnity.EventRef]
     public string[] radioEvent;
     FMOD.Studio.EventInstance[] radioInstance;
+    [FMODUnity.EventRef]
+    [SerializeField]
+    private string _backgroundMusic;
+    FMOD.Studio.EventInstance _backgroundEvent;
 
+    [FMODUnity.EventRef]
+    [SerializeField]
+    private string _ambienceSound;
+    FMOD.Studio.EventInstance ambienceEvent;
 
 
     // Start is called before the first frame update
@@ -17,6 +25,11 @@ public class AudioController : MonoBehaviour
         {
 
         }
+        _backgroundEvent = FMODUnity.RuntimeManager.CreateInstance(_backgroundMusic);
+        _backgroundEvent.start();
+
+        ambienceEvent = FMODUnity.RuntimeManager.CreateInstance(_ambienceSound);
+        ambienceEvent.start();
     }
 
     // Update is called once per frame
