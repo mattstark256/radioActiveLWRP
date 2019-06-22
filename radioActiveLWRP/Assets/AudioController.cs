6af9b808b56238f7891478deac11f9025ec69a53
Matt Laptop <1800715@uad.ac.uk>
2019-06-22 13:44:07 +0100

    added radio audio

---
+++ radioActiveLWRP/Assets/AudioController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string[] radioEvent;
+    FMOD.Studio.EventInstance[] radioInstance;
+
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        for(int i = 0; i < radioEvent.Length; i++)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ radioActiveLWRP/Assets/RadioAudio.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioAudio : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    [SerializeField]
+    private string radioMusic;
+    FMOD.Studio.EventInstance radioEvent;
+
+    
+    void Awake()
+    {
+        radioEvent = FMODUnity.RuntimeManager.CreateInstance(radioMusic);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(radioEvent, transform, GetComponent<Rigidbody>());
+        radioEvent.start();
+    }
+
+    
+    // This is called when the radio is placed at the correct corruption location.
+    public void ChangeMusic()
+    {
+
+    }
+
+
+    // This is called once the radio has finished destroying the corruption.
+    public void RevertMusic()
+    {
+
+    }
+}
