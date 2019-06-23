a22e5edbc2be8c024aef7f4b53ee062cfccf09a4
Matt Laptop <1800715@uad.ac.uk>
2019-06-23 11:38:06 +0100

    added scream script

---
+++ radioActiveLWRP/Assets/Scream.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scream : MonoBehaviour
+{
+    [SerializeField]
+    private float screamHeight = 5f;
+    [SerializeField]
+    private float minimumScreamInterval = 5f;
+
+    private float timeSinceLastScream = 0;
+
+    
+    void Update()
+    {
+        timeSinceLastScream += Time.deltaTime;
+
+        if (transform.position.y < screamHeight && timeSinceLastScream > minimumScreamInterval)
+        {
+            DoScream();
+            timeSinceLastScream = 0;
+        }
+    }
+
+    private void DoScream()
+    {
+        Debug.Log("AAAAAAAAAAAAAAAAAA");
+    }
+}
