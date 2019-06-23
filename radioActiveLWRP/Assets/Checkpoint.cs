42a7f99c61f60ab0602b6093c61d9364096b7af0
Matt Laptop <1800715@uad.ac.uk>
2019-06-23 11:20:19 +0100

    applied corruption material to entire level

---
+++ radioActiveLWRP/Assets/Checkpoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField]
+    private Transform respawnPoint;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController playerController = other.GetComponent<PlayerController>();
+        if (playerController != null)
+        {
+            Respawnable respawnable = other.GetComponent<Respawnable>();
+            if (respawnable != null)
+            {
+                respawnable.SetRespawnPoint(respawnPoint.position);
+            }
+        }
+    }
+}
